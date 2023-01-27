e533dff917585ded96400f797df6c83035320bd1
Alisson Chiquitto <chiquitto@gmail.com>
2023-01-27 15:06:46 -0400

    Improvements in isMirtronCsv

--- isMirtronCsv.py
+++ isMirtronCsv.py
@@ -1,7 +1,12 @@
 #! /usr/bin/env python3
 
-"""predict whether it is mirtron.  "yes" means "is mirtron".
-   "no" means "is not a mirtron"
+# Use CSV with id and seq headers
+# See README.md
+# Use with conda environment
+# conda run -p .condaenv python isMirtronCsv.py --csv sample.csv
+
+"""predict whether it is mirtron.  "1" means "is mirtron".
+   "0" means "is not a mirtron"
 """
 
 import tensorflow as tf
@@ -16,7 +21,9 @@ import getopt
 import numpy as np
 import csv
 import os
- 
+
+BATCH_SIZE_RESULT = 1000
+
 # hyperparameters
 #LR = 0.001       #learning rate
 #TRAINING_ITER = 10000   #iteration times
@@ -31,12 +38,6 @@ FC_SIZE = 128     #nodes of full-connection layer
 NUM_CLASSES = 2   # classification number
  
 # DROPOUT_KEEP_PROB = 0.5   #keep probability of dropout
-
-
-# placeholder
-#input_X = tf.placeholder(tf.float32,[None,SEQUENCE_LENGTH,EMBEDDING_SIZE,1])
-#input_y = tf.placeholder(tf.float32,[None, NUM_CLASSES])
-#keep_prob = tf.placeholder(tf.float32)
 
 x_cast = {"A":[[1],[0],[0],[0]],"U":[[0],[1],[0],[0]],\
           "T":[[0],[1],[0],[0]],"G":[[0],[0],[1],[0]],\
@@ -75,6 +76,31 @@ def seq_vectorize(processed_seq):
   vectorized_seq = vectorized_seq.reshape([1,SEQUENCE_LENGTH,EMBEDDING_SIZE,1])
   return vectorized_seq
 
+def flush_results():
+  csvfile_writer = open(csvoutput, 'a', newline='')
+  csvwriter = csv.DictWriter(csvfile_writer, fieldnames=fieldnames)
+  csvwriter.writerows(results)
+  csvfile_writer.close()
+
+def create_tf_session():
+  sess = tf.Session(config=sess_config)
+
+  # restore the trained model
+  saver = tf.train.import_meta_graph('logs/filter6/filter6.ckpt.meta')
+  # print("graph restore succeed")
+  saver.restore(sess,tf.train.latest_checkpoint("logs/filter6/"))
+  # print("parameters restore succeed")
+
+  predict_result = tf.get_collection('pred_network')[0]
+  graph = tf.get_default_graph()
+
+  # get the placeholder in the restored graph by the method of 
+  # get_operation_by_name()
+  input_X = graph.get_operation_by_name('input_X').outputs[0]
+  keep_prob = graph.get_operation_by_name('keep_prob').outputs[0]
+
+  return sess, predict_result, input_X, keep_prob
+
 try:
   opts, args = getopt.getopt(sys.argv[1:],"hs:",["help","csv="])
 except getopt.GetoptError:
@@ -95,54 +121,62 @@ for op, value in opts:
     sys.exit()
 
 results = []
-fieldnames = []
 
-with tf.Session() as sess:
+# Open CSV input
+csvinput = open(input_path, newline='')
+csvreader = csv.DictReader(csvinput, delimiter=',', quotechar='"')
 
-  # restore the trained model
-  saver = tf.train.import_meta_graph('logs/filter6/filter6.ckpt.meta')
-  # print("graph restore succeed")
-  saver.restore(sess,tf.train.latest_checkpoint("logs/filter6/"))
-  # print("parameters restore succeed")
+# Create CSV output
+fieldnames = ['id', 'seq', 'ismirtron']
+csvoutput = os.path.splitext(input_path)[0] + '_cnnmirtronpred.csv'
 
-  predict_result = tf.get_collection('pred_network')[0]
-  graph = tf.get_default_graph()
+csvfile_writer = open(csvoutput, 'w', newline='')
+csvwriter = csv.DictWriter(csvfile_writer, fieldnames=fieldnames)
+csvwriter.writeheader()
+csvfile_writer.close()
 
-  # get the placeholder in the restored graph by the method of 
-  # get_operation_by_name()
-  input_X = graph.get_operation_by_name('input_X').outputs[0]
-  keep_prob = graph.get_operation_by_name('keep_prob').outputs[0]
+results = []
+result_counter = 0
 
-  with open(input_path, newline='') as csvfile:
-    csvreader = csv.DictReader(csvfile, delimiter=',', quotechar='"')
-    for n, row in enumerate(csvreader):
+# Session preparation
+sess = None
+sess_config = tf.ConfigProto() 
+sess_config.gpu_options.allow_growth = True 
+# sess = tf.Session(config=sess_config)
 
-      if n == 0:
-        fieldnames = list(row.keys()) + ['ismirtron']
+for n, row in enumerate(csvreader):
 
-      processed_seq = seq_process(row['seq'])
-      vectorized_seq = seq_vectorize(processed_seq)
+  # Session creation
+  if sess is None:
+    tf.reset_default_graph()
+    sess, predict_result, input_X, keep_prob = create_tf_session()
+  
+  processed_seq = seq_process(row['seq'])
+  vectorized_seq = seq_vectorize(processed_seq)
 
-      # feed the new data
-      # print("feed the data start")
-      prediction = sess.run(predict_result,feed_dict={input_X:vectorized_seq,keep_prob:1})
+  prediction = sess.run(predict_result, feed_dict={input_X:vectorized_seq,keep_prob:1})
 
-      # print("feed the data end")
-      # print ("prediction:",prediction)
-      # print the predicted class
-      m_index = sess.run(tf.argmax(prediction,1))
-      # if m_index == 0:
-      #   print("Yes,it is a mirtron.")
-      # elif m_index == 1:
-      #   print("No,it is not a mirtron.")
-      row['ismirtron'] = 1 if m_index == 0 else 0
+  # print("feed the data end")
+  # print ("prediction:",prediction)
+  # print the predicted class
+  m_index = sess.run(tf.argmax(prediction,1))
+  # if m_index == 0:
+  #   print("Yes,it is a mirtron.")
+  # elif m_index == 1:
+  #   print("No,it is not a mirtron.")
+  row['ismirtron'] = 1 if m_index == 0 else 0
 
-      results.append(row)
+  results.append(row)
+  result_counter += 1
 
-output_path = os.path.splitext(input_path)[0] + '_cnnmirtronpred.csv'
-with open(output_path, 'w', newline='') as csvfile:
-  # fieldnames = ['first_name', 'last_name']
-  writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+  if result_counter == BATCH_SIZE_RESULT:
+    flush_results()
 
-  writer.writeheader()
-  writer.writerows(results)
+    results.clear()
+    result_counter = 0
+    sess = None
+
+flush_results()
+
+# Close handlers
+csvinput.close()
